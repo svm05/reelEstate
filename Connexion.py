97c8df8b1db2f0aef00c9c77e4ce7fe7c8066ad0
Rhythm Jain <rhythmjain2022@vitbhopal.ac.in>
2025-07-10 10:05:32 +0530

    Updated Connexion.py

--- Connexion.py
+++ Connexion.py
@@ -2,8 +2,25 @@ import pymysql
 
 class Dbconnect(object):
     def __init__(self):
-        self.dbconection = pymysql.connect(host='localhost',port=3308, user='root',passwd='', db='realestate')
+        self.dbconection = pymysql.connect(host='localhost', port=3308, user='root', passwd='', db='realestate')
         self.dbcursor = self.dbconection.cursor()
+        self._ensure_table_exists()
+
+    def _ensure_table_exists(self):
+        self.dbcursor.execute("""
+            CREATE TABLE IF NOT EXISTS house (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                location TEXT,
+                status TEXT,
+                price TEXT,
+                owner TEXT,
+                bed TEXT,
+                bath TEXT,
+                sqft TEXT,
+                sqft_lot TEXT
+            )
+        """)
+        self.dbconection.commit()
 
     def commit_db(self):
         self.dbconection.commit()
